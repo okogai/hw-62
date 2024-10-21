5aa4764054df7081b3b8d962e0dda9b592deab55
Oleg <evolrn89@gmail.com>
2024-10-21 17:17:01 +0600

    Created page Reviews

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import Header from './components/Header/Header.tsx';
 import Footer from './components/Footer/Footer.tsx';
 import AboutUs from './containers/AboutUs/AboutUs.tsx';
 import OurStrategies from './containers/OurStrategies/OurStrategies.tsx';
+import Reviews from './containers/Reviews/Reviews.tsx';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
       <Header/>
-        <AboutUs/>
-        <OurStrategies/>
+      <Routes>
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/our-strategies" element={<OurStrategies />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
       <Footer/>
     </>
   );
